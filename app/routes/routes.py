593263618cbc98d4ee68fc8e5bd80d7af0836be9
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-11 10:14:11 +0100

    modified: routes.py

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,3 +1,27 @@
+from flask import render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import FileField
+import os
+import pyttsx3
+from PyPDF2 import PdfReader
+
+from app import app
+
+class UploadForm(FlaskForm):
+    pdf_file = FileField('PDF File')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UploadForm()
+
+    if form.validate_on_submit():
+        pdf_file = form.pdf_file.data
+        audiobook_path = convert_pdf_to_audio(pdf_file)
+        flash('Audiobook generated successfully!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form)
+
 def convert_pdf_to_audio(pdf_file):
     output_path = 'output.mp3'
     text = extract_text_from_pdf(pdf_file)
