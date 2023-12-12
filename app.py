c0a6aace5e728447d654512c0ab31a61a8ddd5a7
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-12 19:54:16 +0100

    modify: app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ this python module defines the logic and routes for pdf-to-audiobook
 """
 import os
 import tempfile
-from flask import (Flask, render_template, send_file)
+from flask import Flask, render_template, send_file
 import pyttsx3
 from cs50 import SQL
 
@@ -22,35 +22,35 @@ def root():
     """
     this function handles the route for the root url
     """
-    return "Yo, this is the app"
+    return render_template("index.html")
 
 # decorator for defining the convert url
-@app.route("/convert")
-def convert():
+@app.route("/upload")
+def upload():
     """
     this function handles the route for the convert url
     """
     return "Yo, this is for conversion"
 
-@app.route("/tts")
-def tts():
-    text = "Fine weather we've got here, no?"
+# @app.route("/tts")
+# def tts():
+#     text = "Fine weather we've got here, no?"
 
-    # Use a temporary file to store the TTS audio
-    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_audio:
-        temp_filename = temp_audio.name
-        engine.save_to_file(text, temp_filename)
-        engine.runAndWait()
+#     # Use a temporary file to store the TTS audio
+#     with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_audio:
+#         temp_filename = temp_audio.name
+#         engine.save_to_file(text, temp_filename)
+#         engine.runAndWait()
 
-    # Set the appropriate headers for the response
-    response_headers = {
-        'Content-Type': 'audio/mpeg',
-        'Content-Disposition': f'attachment; filename={temp_filename}'
-    }
+#     # Set the appropriate headers for the response
+#     response_headers = {
+#         'Content-Type': 'audio/mpeg',
+#         'Content-Disposition': f'attachment; filename={temp_filename}'
+#     }
 
-    # Send the file as a response
-    return send_file(temp_filename, as_attachment=True, download_name="test.mp3")
+#     # Send the file as a response
+#     return send_file(temp_filename, as_attachment=True, download_name="test.mp3")
 
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
