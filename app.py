5378fc39d459d984d5df5b492ff6f95a4fde3f5b
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-12 18:51:48 +0100

    modified: app.py modified: requirements.txt

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 """
 this python module defines the logic and routes for pdf-to-audiobook
 """
-from flask import Flask, render_template
-# from cs50 import SQL
+import os
+import tempfile
+from flask import (Flask, render_template, send_file)
+import pyttsx3
+from cs50 import SQL
 
 # configure app
 app = Flask(__name__)
 
 # configure database using cs50 library
 # db = SQL()
+
+# instantiate pyttsx3 object
+engine = pyttsx3.init()
 
 # decorator for defining the root url
 @app.route("/")
@@ -25,6 +31,26 @@ def convert():
     this function handles the route for the convert url
     """
     return "Yo, this is for conversion"
+
+@app.route("/tts")
+def tts():
+    text = "Fine weather we've got here, no?"
+
+    # Use a temporary file to store the TTS audio
+    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_audio:
+        temp_filename = temp_audio.name
+        engine.save_to_file(text, temp_filename)
+        engine.runAndWait()
+
+    # Set the appropriate headers for the response
+    response_headers = {
+        'Content-Type': 'audio/mpeg',
+        'Content-Disposition': f'attachment; filename={temp_filename}'
+    }
+
+    # Send the file as a response
+    return send_file(temp_filename, as_attachment=True, download_name="test.mp3")
+
 
 if __name__ ==  "__main__":
     app.run(debug=True)
