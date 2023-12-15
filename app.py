404d69e617a7f40297e1327cf54cfa6226b4fd76
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-15 09:43:12 +0100

    modify: .gitignore (make .gitgnore ignore .gitignore) modify: README.md (clearer instructions) modify: app.py (define conversion logic, housekeeping/refactoring) modify: templates/index.html (change "Upload" to "Convert")

--- app.py
+++ app.py
@@ -1,99 +1,94 @@
-"""
-this python module defines the logic and routes for pdf-to-audiobook
-"""
+"""logic and routes for pdf-to-audiobook"""
 import os
 import sys
 import secrets
 
-# import werkzeug
 from flask import Flask, redirect, request, render_template, url_for
 import flask_uploads as fu
 
-# import pyttsx3
+import pyttsx3
 import pdfminer.high_level as pm
-# from cs50 import SQL
-
-# import utils
 
 app = Flask(__name__)
 
-# uploadset config
-pdfs = fu.UploadSet("pdfs", extensions="pdf")
+pdfs = fu.UploadSet("pdfs", extensions="pdf") # uploadset config
 
-# app.config["UPLOADED_PDFS_DEST"] = utils.set_destination(app, path="uploads/")
 app.config["UPLOADED_PDFS_DEST"] = "uploads/"
 app.config["MAX_CONTENT_LENGTH"] = 3 * 1024 * 1024 # set max upload size to 3 megabytes
 app.config["SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
 
-def 
+fu.configure_uploads(app, pdfs) # store pdf uploadset config in app instance
 
-# store pdf uploadset config in app instance
-fu.configure_uploads(app, pdfs)
+engine = pyttsx3.init() # instantiate pyttsx3 object
 
-# configure database using cs50 library
-# db = SQL()
+# define file processing functions for readability
+def extract_text_from_pdf(pdf_path):
+    """ extract text using pdfminer.six """
+    return pm.extract_text(pdf_path)
 
-# instantiate pyttsx3 object
-# engine = pyttsx3.init()
+def save_string_to_text_file(text, text_path):
+    """ write a string variable (text) to a text file (@ text_path) """
+    with open(text_path, "w", encoding="utf-8") as text_file:
+        text_file.write(text)
 
-# add logic for parsing pdf
+def text_file_checker(text, text_path):
+    """ compare string variable and text file """
+    if os.path.exists(text_path):
+        # read text file
+        with open(text_path, "r", encoding="utf-8") as file:
+            file_content = file.read()
 
-# add logic for converting, saving text to audio
+        # compare the stripped versions of both files
+        if text.strip() == file_content.strip():
+            print(len(text.strip()))
+            print(len(file_content.strip()))
+            print("Success! The string written to text file.")
+        else:
+            print(len(text.strip()))
+            print(len(file_content.strip()))
+            print("Warning! Text file content differs from the original string.")
+    # catch-all exception
+    else:
+        print("Failure! File not created.")
+
+def convert_text_to_audio(text, audio_path):
+    """ function to create audiobook using pyttsx3 """
+    engine.save_to_file(text, type="unicode", filename=audio_path)
+    return
+    
 
 # decorator for defining the root url
 @app.route("/")
 def root():
-    """
-    this function handles the route for the root url
-    """
-
-    test2 = __name__ == "__main__"
-    print(test2)
+    """ function to handle the root url """
     return render_template("index.html")
 
 @app.route("/", methods=["GET", "POST"])
-def upload():
-    """
-    this function handles pdf uploads
-    """
+def convert():
+    """ function to handle pdf uploads """
 
     if request.method == "POST" and "pdf" in request.files:
-        try:
-            # check if a file exists
-            if not request.files["pdf"]:
-                redirect(url_for("root", error="no_file"))
+        # check if file is present
+        if not request.files["pdf"]:
+            redirect(url_for("root", error="no_file"))
 
-            # save pdf upload to the "uploads" folder
+        try:
+            # save pdf in "uploads", store name in filename
             filename = pdfs.save(request.files["pdf"])
 
-            file_path = "uploads/" + filename
+            print(f"{filename} was saved!")
 
-            text = pm.extract_text(file_path)
+            pdf_path = "uploads/" + filename
 
-            with open("uploads/pdf.txt", "w", encoding="utf-8") as text_file:
-                # write FILE variable to a txt file
-                text_file.write(text)
+            text = extract_text_from_pdf(pdf_path)
 
-            # check if the file exists
-            if os.path.exists(file_path):
-                # read text file
-                with open(file_path, "r", encoding="utf-8") as file:
-                    file_content = file.read()
+            text_path = "uploads/pdf.txt"
 
-                # compare the stripped versions of both files
-                if text.strip() == file_content.strip():
-                    print(len(text.strip()))
-                    print(len(file_content.strip()))
-                    print("Success! The string written to text file.")
-                else:
-                    print(len(text.strip()))
-                    print(len(file_content.strip()))
-                    print("Warning! Text file content differs from the original string.")
-            # catch-all exception
-            else:
-                print("Failure! File not created.")
+            save_string_to_text_file(text, text_path)
 
-            print(f'haha, {filename} saved!')
+            text_file_checker(text, text_path)
+
+            print(f"haha, {filename} saved!")
 
             return "Your file uploaded successfully (Not really)!"
         except fu.UploadNotAllowed:
@@ -102,17 +97,13 @@ def upload():
 
 @app.errorhandler(413)
 def file_size_limit_exceeded(error):
-    """
-    test for 413 error handling
-    """
+    """ error handling for 413 """
     return redirect(url_for("root", error="file_size_limit_exceeded"))
 
 @app.errorhandler(404)
 def not_found(error):
-    """
-    test for 404 error handling
-    """
+    """ error handling for 404 """
     return redirect(url_for("root", error="not_found"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
