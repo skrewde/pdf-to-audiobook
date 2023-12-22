5412a5b10d340d045ac482432e57d0412570c72e
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-22 20:26:57 +0100

    add: test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+"""
+module to test page count function?
+"""
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdftypes import resolve1
+
+with open('/upload/file.pdf', 'rb') as f:
+    parser = PDFParser(f)
+    doc = PDFDocument(parser)
+
+    parser.set_document(doc)
+    pages = resolve1(doc.catalog['Pages'])
+    pages_count = pages.get('Count', 0)
