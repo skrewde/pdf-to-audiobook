5aba9d55df761584084816fe43e0bb51d5ca3683
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-23 19:47:54 +0100

    test for pymupdf

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
 """
 module to test page count function?
 """
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdftypes import resolve1
+import fitz
 
-with open('/uploads/test.pdf', 'rb') as f:
-    parser = PDFParser(f)
-    doc = PDFDocument(parser)
+doc = fitz.open("uploads/test.pdf") # open a document
 
-    parser.set_document(doc)
-    pages = resolve1(doc.catalog['Pages'])
-    pages_count = pages.get('Count', 0)
+out = open("test.txt", "wb") # create a text output
+
+for page in doc: # iterate the document pages
+    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
+    out.write(text) # write text of page
+    out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
+    
+out.close()
+
+
+
+
+# from pdfminer.pdfdocument import PDFDocument
+# from pdfminer.pdfparser import PDFParser
+# from pdfminer.pdftypes import resolve1
+
+# with open('/uploads/test.pdf', 'rb') as f:
+#     parser = PDFParser(f)
+#     doc = PDFDocument(parser)
+
+#     parser.set_document(doc)
+#     pages = resolve1(doc.catalog['Pages'])
+#     pages_count = pages.get('Count', 0)
