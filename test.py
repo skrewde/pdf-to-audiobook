74c98811a3b8bd201efcc38d167c05d16cfd06d3
skrewde <94148640+skrewde@users.noreply.github.com>
2023-12-23 20:34:13 +0100

    add tts function

--- test.py
+++ test.py
@@ -2,8 +2,17 @@
 module to test page count function?
 """
 import fitz
+import pyttsx3
+
+engine = pyttsx3.init() # instantiate pyttsx3 object
 
 doc = fitz.open("uploads/test.pdf") # open a document
+
+def convert_text_to_audio(text, audio_path):
+    """ create "audiobook" using pyttsx3 """
+    engine.save_to_file(text, filename=audio_path)
+    engine.runAndWait()
+
 
 out = open("test.txt", "wb") # create a text output
 
@@ -11,11 +20,9 @@ for page in doc: # iterate the document pages
     text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
     out.write(text) # write text of page
     out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
-    
-out.close()
+    convert_text_to_audio(text, "uploads/test.mp3")
 
-
-
+# out.close()
 
 # from pdfminer.pdfdocument import PDFDocument
 # from pdfminer.pdfparser import PDFParser
